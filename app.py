2060688000f5576c8d45c6dea90da50a0c04d772
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-08-09 19:35:48 +0330

    add rating_small

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import pandas as pd
 import ast
 
 # Load the data
-df = pd.read_csv("data/movies_metadata.csv")
+df_metadata = pd.read_csv("data/movies_metadata.csv")
+df_rating = pd.read_csv("data/ratings_small.csv")
 
 # Function to safely parse JSON/string-represented lists
 def parse_json_column(col):
@@ -18,8 +19,8 @@ def parse_json_column(col):
             return []
 
 # Apply to genres column
-df['genres'] = df['genres'].apply(parse_json_column)
+df_metadata['genres'] = df_metadata['genres'].apply(parse_json_column)
 
 # Now you can extract genre names
-df['genre_names'] = df['genres'].apply(lambda x: [g['name'] for g in x] if isinstance(x, list) else [])
-print(df['genre_names'].head())
+df_metadata['genre_names'] = df_metadata['genres'].apply(lambda x: [g['name'] for g in x] if isinstance(x, list) else [])
+print(df_metadata['genre_names'].head())
