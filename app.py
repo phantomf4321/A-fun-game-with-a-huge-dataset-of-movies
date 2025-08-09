a5eecc4b5c22bc82dbf706def7cc3222303b262d
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-08-09 19:23:57 +0330

    reading movies_metadata extract geres

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import json
 df = pd.read_csv("data/movies_metadata.csv")
 df['genres_id'] = df['genres'].str['id']
 df['genres_name'] = df['genres'].str['name']
-for g in df['genres']:
-    print(g)
+for raw in df['genres']:
+    genres = [g["name"] for g in json.loads(raw)]
+    print(genres)
 
