e43f6b671ac9d0a117beaf7d66de5859abb8da80
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-08-09 19:48:30 +0330

    add rating_small

---
+++ main.py
@@ -0,0 +1,10 @@
+from app.functions import Datasets
+
+metadata = Datasets("data/movies_metadata.csv")
+
+"""# Apply to genres column
+df_metadata['genres'] = df_metadata['genres'].apply(parse_json_column)
+
+# Now you can extract genre names
+df_metadata['genre_names'] = df_metadata['genres'].apply(lambda x: [g['name'] for g in x] if isinstance(x, list) else [])
+print(df_metadata['genre_names'].head())"""
