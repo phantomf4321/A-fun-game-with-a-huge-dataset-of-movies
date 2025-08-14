c21a971d346ef32538518775ad7c6806033c14b9
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-08-14 19:10:05 +0330

    EDA all in one ExploratoryDataAnalysis test on main

--- main.py
+++ main.py
@@ -4,11 +4,4 @@ import pandas as pd
 from ExploratoryDataAnalysis import EDA
 
 
-# --- 1) Compute movie-level stats from cleaned ratings ---
-movie_stats = (
-    r_full.groupby(["tmdbId", "title"], as_index=False)
-          .agg(
-              Ri=("rating", "mean"),   # mean rating
-              vi=("rating", "count")   # vote count
-          )
-)
+eda = EDA()
