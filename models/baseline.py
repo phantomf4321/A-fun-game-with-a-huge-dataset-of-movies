de4095ea03e40e6b260f176294ae4fe6d5f03685
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-08-27 21:12:16 +0330

    base line

--- models/baseline.py
+++ models/baseline.py
@@ -2,3 +2,17 @@ class Baseline():
     def __init__(self, r_full):
         self.r_full = r_full
         print("Bsaseline is constructed...")
+
+        self.movie_stats = (
+            self.r_full.groupby(["tmdbId", "title"], as_index=False)
+            .agg(
+                Ri=("rating", "mean"),  # mean rating
+                vi=("rating", "count")  # vote count
+            )
+        )
+
+        # attach genres list (first occurrence)
+        self.movie_stats["genres"] = self.movie_stats["tmdbId"].map(
+            self.r_full.drop_duplicates("tmdbId")
+            .set_index("tmdbId")["genres"]
+        )
