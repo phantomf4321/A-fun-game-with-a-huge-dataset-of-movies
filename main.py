593a8d3fc7550190dcf2cb836ccfa173e91bc4ad
Sajjad <sajjadranjbaryazdi@gmail.com>
2025-08-27 21:18:00 +0330

    base line top_10_globally_popular_movies

--- main.py
+++ main.py
@@ -12,37 +12,9 @@ from ExploratoryDataAnalysis import EDA
 
 eda = EDA()
 r_full = eda.get_r_full()
-# --- 1) Compute movie-level stats from cleaned ratings ---
-movie_stats = (
-    r_full.groupby(["tmdbId", "title"], as_index=False)
-          .agg(
-              Ri=("rating", "mean"),   # mean rating
-              vi=("rating", "count")   # vote count
-          )
-)
-
-# attach genres list (first occurrence)
-movie_stats["genres"] = movie_stats["tmdbId"].map(
-    r_full.drop_duplicates("tmdbId")
-          .set_index("tmdbId")["genres"]
-)
 
 
-# --- 2) IMDb WR global baseline ---
-# Global C = weighted mean of all ratings
-C = (movie_stats["Ri"] * movie_stats["vi"]).sum() / movie_stats["vi"].sum()
 
-# m = 80th percentile of vote counts
-m = np.quantile(movie_stats["vi"], 0.80)
-
-# WR formula
-v = movie_stats["vi"]
-R = movie_stats["Ri"]
-movie_stats["WR"] = (v / (v + m)) * R + (m / (v + m)) * C
-
-# Keep movies with v >= m
-global_wr = movie_stats[movie_stats["vi"] >= m].copy()
-global_wr = global_wr.sort_values("WR", ascending=False)
 
 print("Global WR parameters:")
 print({"C": round(C, 3), "m": int(m), "m_quantile": 0.80})
--- models/baseline.py
+++ models/baseline.py
@@ -1,8 +1,10 @@
+import numpy as np
 class Baseline():
     def __init__(self, r_full):
         self.r_full = r_full
         print("Bsaseline is constructed...")
 
+        # --- Compute movie-level stats from cleaned ratings ---
         self.movie_stats = (
             self.r_full.groupby(["tmdbId", "title"], as_index=False)
             .agg(
@@ -16,3 +18,28 @@ class Baseline():
             self.r_full.drop_duplicates("tmdbId")
             .set_index("tmdbId")["genres"]
         )
+
+    def top_10_globally_popular_movies(self):
+        # --- 2) IMDb WR global baseline ---
+        # Global C = weighted mean of all ratings
+        C = (self.movie_stats["Ri"] * self.movie_stats["vi"]).sum() / self.movie_stats["vi"].sum()
+
+        # m = 80th percentile of vote counts
+        m = np.quantile(self.movie_stats["vi"], 0.80)
+
+        # WR formula
+        v = self.movie_stats["vi"]
+        R = self.movie_stats["Ri"]
+        self.movie_stats["WR"] = (v / (v + m)) * R + (m / (v + m)) * C
+
+        # Keep movies with v >= m
+        global_wr = self.movie_stats[self.movie_stats["vi"] >= m].copy()
+        global_wr = global_wr.sort_values("WR", ascending=False)
+
+        resaults = {
+            "C": C,
+            "m": m,
+            "global_wr": global_wr
+        }
+
+        return resaults
